0e663d2093de3a78e4b43691aa317b5d94628879
SPZHunter <s.pz.hunter@gmail.com>
2022-08-12 20:41:24 +0100

    Made progress on the scraper - see desc.
    Scraper currently uses BS4 and then passes to a json. This takes a long time. The YGOProdeck API actually just provides html, no JS, so it could be fine to just use requests.
    
    Passing to JSON also seems to be slow, so maybe it's best to give scraper options for what to extract rather than taking the whole html and parsing it.

--- Tools/YGO_Pro_Deck_Scraper.py
+++ Tools/YGO_Pro_Deck_Scraper.py
@@ -21,13 +21,86 @@ Version:    0.1
 #       0.1        |   11/08/2022    |                                       #
 #----------------------------------------------------------------------------#
 """
-
-import requests
+#--------Imports
+import json
+from pathlib import Path
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
+# Homemade
+from tools.config_reader import csv_to_dict
 
-# Assign the web driver using the driver in this project's repository
-driver = Chrome(executable_path=r'webdrivers\chromedriver')
+#--------Scraper object
+class scraper(object):
+    """
+    """
+    
+    def __init__(self, **kw):
+        
+        # webdriver path passed in
+        webdriversDir = kw["webdriversDir"]
+        
+        # Set up the driver
+        # if selected browser is chrome
+        if kw["browser"].title() == "Chrome": 
+            options = ChromeOptions()
+            options.headless = True
+            self.__driver = Chrome(executable_path=webdriversDir + '\chromedriver', 
+                        options=options)
+        # Default to using chrome
+        else:
+            options = ChromeOptions()
+            options.headless = True
+            self.__driver = Chrome(executable_path=webdriversDir + '\chromedriver', 
+                        options=options)
+        
+       
+        # Assign the URL
+        self.__url = kw["url"]
+        
+        # Get the web page
+        print(f"requesting from: {self.__url}")
+        self.__driver.get(self.__url)
+        
+        # Create beautiful soup object
+        print("parsing to beautiful soup 4")
+        soup = bs(self.__driver.page_source, 'lxml')
+        
+        # Parse soup to JSON
+        print("parsing to json")
+        self.__json = json.loads(soup.get_text())
+        
+        
+    @property
+    def json(self):
+        return self.__json
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
